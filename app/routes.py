07447b761bb12a4e5608c1debdb7a63dfd7f9f6d
Sazid AL Emon <md.sazid.al.emon@gmail.com>
2024-08-26 17:03:00 +0600

    registration page

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,9 @@ from app.models import User
 #handle login-logout view
 from flask_login import current_user,login_user,logout_user,login_required
 
+#handle registration view
+from app.forms import RegistrationForm
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -31,6 +34,20 @@ def index():
         }
     ]
     return render_template('index.html',title='Home Page',posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET','POST'])
