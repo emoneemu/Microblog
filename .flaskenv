7e486ca67ba59fa3537ea51e9aafdf0de7170586
Sazid AL Emon <md.sazid.al.emon@gmail.com>
2024-09-06 08:43:23 +0600

    Translator integrating

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,2 @@
 FLASK_APP = blogproject.py
 FLASK_DEBUG=0
-MS_TRANSLATOR_KEY=''
--- blogproject.py
+++ blogproject.py
@@ -2,7 +2,7 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 #That initial line changed to this for creating a new shell context for db
-from app import app,db
+from app import app,db,cli
 from app.models import User,Post
 
 #The Initial single line was for blogproject
